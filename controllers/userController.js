21a0d09e835f64d5d5bb98881765c6ebb27bdf49
lucasmcmatos <lucasmmatos05@gmail.com>
2023-05-18 10:55:21 -0300

    Project Created

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,17 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const {loginValidate , registerValidate} = require('./validate');
 
 const userController = {
     register: async function (req , res) {
 
-        const selectedUser = await User.findOne({email: req.body.email})
+        const {error} = registerValidate(req.body)
+        if(error){
+            return res.status(400).send(error.message)
+        }
 
+        const selectedUser = await User.findOne({email: req.body.email})
         if(selectedUser) {
             return res.status(400).send('Email alredy exist!')
         }
@@ -26,6 +31,13 @@ const userController = {
 
     },
     login: async function (req,res){
+
+        const {error} = loginValidate(req.body);
+        if(error){
+            return res.status(400).send(error.message)
+        }
+
+
         const selectedUser = await User.findOne({email: req.body.email})
 
         if(!selectedUser) {
